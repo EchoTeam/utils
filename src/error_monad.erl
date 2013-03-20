9e4f4fc53ee46d5ea6fab9d1274122e95f728015
Vlad Skvortsov <vss@73rus.com>
2013-03-20 17:35:24 +0000

    Initial commit

---
+++ src/error_monad.erl
@@ -0,0 +1,39 @@
+-module(error_monad).
+-export([
+  break/1,
+  do/1,
+  do/2,
+  rdo/2,
+  error/1,
+  error/2,
+  error/3,
+  ok/1
+]).
+
+% Poor man's Error monad.
+do(F) -> do(F, undefined).
+
+do([], Arg) -> Arg;
+
+do([Fun | RestFuns], Arg) ->
+  case Fun(Arg) of
+    {error, Error} -> Error;
+    {break, Data} -> Data;
+    {success, V} -> do(RestFuns, V);
+    V -> do(RestFuns, V)
+  end.
+
+% reversed arguments do
+rdo(Arg, Funs) -> do(Funs, Arg).
+
+% "Fail" functions. The application will see {error, A, B, ...} value extracted
+% out of the monad.
+error(A) -> {error, {error, A}}.
+error(A, B) -> {error, {error, A, B}}.
+error(A, B, C) -> {error, {error, A, B, C}}.
+
+% "Success" function. No interruption. The next function gets A.
+ok(A) -> {success, A}.
+
+% "Interrupts" functions. 
+break(A) -> {break, A}.
